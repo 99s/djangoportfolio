d01e6acb096fa2ed90ff0f7676d02bc3585b4cf9
Droy <divi9e@gmail.com>
2019-01-13 10:25:29 +0530

    ImageWar

--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from PIL import Image
+
 
 # Create your models here.
 
+# Use JS to upload fixed size image
+
+
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/',
+        null=True,
+        blank=True,
+        editable=True)
     summary = models.CharField(max_length=200)
+    
+
+    
