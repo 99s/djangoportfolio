5bf9dc0bf9fef72728d6d455165bf02c18359bb4
Droy <divi9e@gmail.com>
2019-01-13 13:28:06 +0530

    blogalpha1

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include, url
 import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='homePage')
+    path('', jobs.views.home, name='homePage'),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
